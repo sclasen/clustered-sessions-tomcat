45da77f9240d74c2ab668b201beb3f8975cfac3a
Scott Clasen <scott@heroku.com>
2012-01-31 13:50:32 -0800

    add weight to sessions

--- src/main/java/servlet/SessionObj.java
+++ src/main/java/servlet/SessionObj.java
@@ -8,6 +8,11 @@ import java.io.Serializable;
  * MSM silently fails on non serializable
  */
 public class SessionObj implements Serializable {
+
+    //IMPORTANT
+    static final long serialVersionUID = 1L;
+
     public int count;
     public long last;
+    public byte[] weight;
 }
--- src/main/java/servlet/SessionServlet.java
+++ src/main/java/servlet/SessionServlet.java
@@ -7,6 +7,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Random;
 
 @WebServlet(
         name = "SessionServlet",
@@ -20,6 +21,15 @@ public class SessionServlet extends HttpServlet {
         SessionObj s = (SessionObj) req.getSession().getAttribute("x");
         if (s == null) {
             s = new SessionObj();
+        }
+        String weight = req.getParameter("weight");
+        if (weight != null) {
+            System.out.println("weight:" + weight);
+            int w = Integer.parseInt(weight) * 1024;
+            byte[] heavy = new byte[w];
+            int set = new Random().nextInt(w);
+            heavy[set] = (byte) set;
+            s.weight = heavy;
         }
         Long current = System.currentTimeMillis();
         Long last = s.last;
